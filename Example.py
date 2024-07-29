31f013de8e84eeb3d2934f5242354cc0d0232f57
Jonathan Ralph Adsetts <141044524+jadsetts@users.noreply.github.com>
2024-07-29 17:48:19 -0400

    Update Example.py

--- Example.py
+++ Example.py
@@ -2,9 +2,9 @@
 
 image=(r'Picture1.jpg')
 resizingScalar=1 #1 is the same image, 4 is 25 % of the original resolution, cannot go below 1 or above height/width.
-threshold=220 #This changes how the binarization occurs.
+threshold=156 #This changes how the binarization occurs.
 imageSizeInmm2=29.607266 #From 'calibrateDistance' function.
-RGBBi='binary' #This could be 'r','g','b','binary'
+RGBBi='r' #This could be 'r','g','b','binary'
 flipBinaryOption = 1 #0 does nothing, 1 inverts binary
 
 pitsInImage(image,resizingScalar,threshold,imageSizeInmm2,RGBBi,flipBinaryOption)
